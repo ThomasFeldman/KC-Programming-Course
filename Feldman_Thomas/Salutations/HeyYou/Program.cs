42a2ed547017c82f328b3f94a72d4013e60603f7
ThomasFeldman <tfeldman1.31@gmail.com>
2021-01-18 23:32:11 -0600

    Week 1 about finished

---
+++ Feldman_Thomas/Salutations/HeyYou/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeyYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, You!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Feldman_Thomas/Salutations/HowdyFolks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowdyFolks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy, Folks!");
+
+            Console.ReadLine();
+        }
+    }
+}
