e867d7156a76732855697f261cd2ece82e15e9c3
ThomasFeldman <tfeldman1.31@gmail.com>
2021-02-01 23:26:07 -0600

    Completed week 3 labs

---
+++ Feldman_Thomas/Pets/ListPets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int petAge;
+
+            Console.WriteLine("Enter the age of your dog: ");
+            petAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your pet is " + DogYears(petAge) + " years old in dog years.");
+            Console.WriteLine("Your pet is " + CatYears(petAge) + " years old in cat years.");
+            Console.ReadLine();
+
+        }
+
+        public static int DogYears(int petAge)
+        {
+            int dogYears = petAge * 7;
+
+            return dogYears;
+
+        }
+
+        public static int CatYears(int petAge)
+        {
+            int catYears = petAge * 4;
+
+            return catYears;
+        }
+    }
+}
---
+++ Feldman_Thomas/Pets/PetInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare vars
+            string petName = "Fido";
+            string humanName = "Tim Smith";
+            int petAge;
+            int humanAge;
+            
+            //First print statement
+            Console.WriteLine( humanName + " has a pet named " + petName + ".");
+          
+            //Enter Human’s age:  
+            Console.WriteLine("Enter " + humanName + "'s age: ");
+            humanAge = int.Parse(Console.ReadLine());
+            
+            //Enter Pet's age
+            Console.WriteLine("Enter " + petName + "'s age: ");
+            petAge = int.Parse(Console.ReadLine());
+
+            //Display and wait
+            Console.WriteLine(humanName + " is " + humanAge + " years old and the pet "
+                + petName + " is " + petAge + " years old.");
+            Console.ReadLine();
+        }
+    }
+}
