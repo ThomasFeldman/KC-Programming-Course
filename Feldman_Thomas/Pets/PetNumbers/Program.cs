73dcb23e8659bf8fb9c5a6ecf631bd74ae84d095
ThomasFeldman <tfeldman1.31@gmail.com>
2021-02-07 23:59:41 -0600

    Another lab done

---
+++ Feldman_Thomas/Pets/PetNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int petNo = 0;
+
+            Console.WriteLine("How many pets do you have?");
+            petNo = Convert.ToInt32(Console.ReadLine());
+
+            switch (petNo)
+            {
+                case 0:
+                    Console.WriteLine("Maybe you will get some pets someday soon!");
+                    break;
+                case 1:
+                    Console.WriteLine("Say hello to your special friend from Thomas!");
+                    break;
+                case 2:
+                    Console.WriteLine("Two is a nice number of pets!");
+                    break;
+                case 3:
+                    Console.WriteLine("You must really like pets!");
+                    break;
+                case 4:
+                    Console.WriteLine("Four is a lot of pets!");
+                    break;
+                default:
+                    if (petNo < 0){
+                        Console.WriteLine("That's not a valid number of pets!");
+                    }
+                    else {
+                        Console.WriteLine("You have so many pets!");
+                    }
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Feldman_Thomas/Pets/PetType/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dog = "N";
+            string cat = "N";
+            string other = "N";
+
+            Console.WriteLine("Do you have a dog (Y/N)?");
+            if (Console.ReadLine() == "Y") {
+                dog = "Y";
+            }
+
+            Console.WriteLine("Do you have a cat (Y/N)?");
+            if (Console.ReadLine() == "Y")
+
+            {
+                cat = "Y";
+            }
+
+            Console.WriteLine("Do you have other types of pets (Y/N)?");
+            if (Console.ReadLine() == "Y")
+            {
+                other = "Y";
+            }
+
+
+            if (dog == "Y")
+            {
+                Console.WriteLine("Congratulations on your pet dog!");
+            }
+
+            if (cat == "Y")
+            {
+                Console.WriteLine("Say hello to your kitty from Thomas");
+            }
+
+            if (other == "Y")
+            {
+                Console.WriteLine("Your pet must be interesting!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
