a0b232697b73ea569a4f6521363d4bec9e718715
ThomasFeldman <tfeldman1.31@gmail.com>
2021-02-16 22:34:40 -0600

    Week 5 done, waiting on Week 6 module

---
+++ Feldman_Thomas/Calendars/Months/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfMonth = 1;
+            
+            //June While Loop
+            while (dayOfMonth < 31) {
+                Console.WriteLine("June " + dayOfMonth);
+                dayOfMonth++;
+            }
+
+            Console.ReadLine();
+            //December Do-While Loop
+            dayOfMonth = 1;
+
+            do
+            {
+                Console.WriteLine("December " + dayOfMonth);
+                dayOfMonth++;
+            } while (dayOfMonth < 32);
+
+            //March For Loop
+            dayOfMonth = 1;
+
+            for(int i = dayOfMonth; i < 32; i++)
+            {
+                Console.WriteLine("March " + i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
